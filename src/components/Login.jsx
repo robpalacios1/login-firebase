8aedeb47990a7f1c6a5dfb8e44859b59213fd905
roberto <robpalacios1@hotmail.com>
2020-10-23 21:17:58 -0500

    add email password validations

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,28 +1,80 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Login = () => {
+
+    const[email, setEmail] = useState('')
+    const[password, setPassword] = useState('')
+    const[error, setError] = useState(null)
+    const[esRegistro, SetEsRegistro] = useState(true)
+
+    const procesarDatos = (e) => {
+        e.preventDefault()
+
+        // Validacion de entrada email
+        if(!email.trim()) {
+            setError('Ingrese Email')
+            return
+        }
+
+        // Validacion de entrada password
+        if(!password.trim()) {
+            setError('Ingrese Password')
+            return
+        }
+        setError(null)
+
+        // Validacion minima de caracteres
+        if(password.length < 6) {
+            setError('Password debe contener minimo 6 caracteres')
+            return
+        }
+    }
+
     return (
         <div className="mt-5">
-            <h3 className="text-center">Acceso o Registro de usuarios</h3>
+            <h3 className="text-center">
+                {
+                    esRegistro ? 'Registro de usuarios' : 'Login de acceso'
+                }
+            </h3>
             <hr />
             <div className="row justify-content-center">
                 <div className="col-12 col-sm-8 col-md-6 col-xl-4">
-                    <form>
+                    <form onSubmit={procesarDatos}>
+                        {
+                            error ? (
+                                <div className="alert alert-danger">
+                                    {error}
+                                </div>
+                            ): null
+                        }
                         <input
                             type="email"
                             className="form-control mb-2"
                             placeholder="Ingrese su email"
+                            onChange={e => setEmail(e.target.value)}
+                            value={email}
                         />
                         <input
                             type="password"
                             className="form-control mb-2"
                             placeholder="Ingrese su contraseña"
+                            onChange={e => setPassword(e.target.value)}
+                            value={password}
                         />
-                        <button className="btn btn-dark btn-lg btn-block">
-                            Registrarse
+                        <button className="btn btn-dark btn-lg btn-block" type="submit">
+                            { 
+                                esRegistro ? "Registrarse" : "Acceder"
+                            }
                         </button>
-                        <button className="btn btn-info btn-sm btn-block">
-                            Ya tienes cuenta?
+                        <button
+                            className="btn btn-info btn-sm btn-block"
+                            type="button"
+                            onClick={() => SetEsRegistro(!esRegistro)}
+                        >
+                            {
+                                esRegistro ? 'Ya estas registrado?' : 'No tienes cuenta?'
+                            }
                         </button>
                     </form>
                 </div>
