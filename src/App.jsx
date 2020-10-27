806c0a6ad947b0d2966d33597d05540028e9238e
roberto <robpalacios1@hotmail.com>
2020-10-26 21:15:36 -0500

    add protected route

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Login from './components/Login'
+import Admin from './components/Admin';
+import {auth} from './firebase'
 
 function App() {
-  return (
+
+  const [firebaseUser, setFirebaseUser] = useState(false)
+
+  useEffect(() => {
+    auth.onAuthStateChanged(user => {
+      console.log(user)
+      if(user) {
+        setFirebaseUser(user)
+      }else{
+        setFirebaseUser(null)
+      }
+    })
+  }, [])
+
+  return firebaseUser !== false ? (
     <Router>
       <div className="container">
         <Navbar />
@@ -16,12 +32,14 @@ function App() {
             <Login />
           </Route>
           <Route path="/admin">
-            admin...
+            <Admin />
           </Route>
         </Switch>
       </div>
     </Router>
-  );
+  ) : (
+    <p>Cargando...</p>
+  )
 }
 
 export default App;
