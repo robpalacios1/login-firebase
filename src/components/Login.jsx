bfa44d43c0d4ca9cc06384be57ee3c9c2af48cee
roberto <robpalacios1@hotmail.com>
2020-10-28 18:21:39 -0500

    add parameters into useState email and password

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -72,6 +72,10 @@ const Login = (props) => {
                 email: res.user.email,
                 uid: res.user.uid
             })
+            await db.collection(res.user.uid).add({
+                name: 'Tarea de ejemplo',
+                fecha: Date.now()
+            })
             setEmail('')
             setPassword('')
             setError(null)
