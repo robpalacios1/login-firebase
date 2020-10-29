15d68f031bbff399b414995b4d22ff5836f33797
roberto <robpalacios1@hotmail.com>
2020-10-29 15:55:53 -0500

    change name of title

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = (props) => {
 
     return (
         <div className="navbar navbar-dark bg-dark">
-            <Link className="navbar-brand" to="/">AUTH</Link>
+            <Link className="navbar-brand" to="/">REACT AUTH</Link>
             <div>
                 <div className="d-flex">
                     <NavLink className="btn btn-dark mr-2" to="/" exact>
